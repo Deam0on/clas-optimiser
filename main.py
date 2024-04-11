4c146c2a4333aab3f6d6227a8ce90a2904e2f0ad
Deam0on <71432962+Deam0on@users.noreply.github.com>
2024-04-11 12:19:17 +0200

    Update main.py

--- main.py
+++ main.py
@@ -25,40 +25,8 @@ from keras.callbacks import EarlyStopping
 from sklearn.preprocessing import LabelEncoder
 from scipy.optimize import minimize
 
-# #Pull from bucket
-# def download_blob(bucket_name, source_blob_name, destination_file_name):
-#     """Downloads a blob from the bucket."""
-#     # The ID of your GCS bucket
-#     # bucket_name = "your-bucket-name"
-
-#     # The ID of your GCS object
-#     # source_blob_name = "storage-object-name"
-
-#     # The path to which the file should be downloaded
-#     # destination_file_name = "local/path/to/file"
-
-#     storage_client = storage.Client()
-
-#     bucket = storage_client.bucket(bucket_name)
-
-#     # Construct a client side representation of a blob.
-#     # Note `Bucket.blob` differs from `Bucket.get_blob` as it doesn't retrieve
-#     # any content from Google Cloud Storage. As we don't need additional data,
-#     # using `Bucket.blob` is preferred here.
-#     blob = bucket.blob(source_blob_name)
-#     blob.download_to_filename(destination_file_name)
-
-#     print(
-#         "Downloaded storage object {} from bucket {} to local file {}.".format(
-#             source_blob_name, bucket_name, destination_file_name
-#         )
-#     )
-    
-# download_blob(uw-nn-storage, DATASET_T2.csv, /home/deamoon_uw_nn/bucket_source)
-# download_blob(uw-nn-storage, DATASET_V2.csv, /home/deamoon_uw_nn/bucket_source)
-
-os.system("gcloud storage cp gs://uw-nn-storage/DATASET_T2.csv /home/deamoon_uw_nn/bucket_source")
-os.system("gcloud storage cp gs://uw-nn-storage/DATASET_V2.csv /home/deamoon_uw_nn/bucket_source")
+# os.system("gcloud storage cp gs://uw-nn-storage/DATASET_T2.csv /home/deamoon_uw_nn/bucket_source")
+# os.system("gcloud storage cp gs://uw-nn-storage/DATASET_V2.csv /home/deamoon_uw_nn/bucket_source")
 
 # Assuming your CSVs are formatted correctly for this task
 TrainingSet = np.genfromtxt("/home/deamoon_uw_nn/bucket_source/DATASET_T2.csv", delimiter=",", skip_header=True)
