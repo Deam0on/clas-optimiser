c4004035330175040bb7434f3dbd77cda539acdc
Deam0on <71432962+Deam0on@users.noreply.github.com>
2024-04-12 12:12:41 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,4 @@
 ## placeholder
+os.system("python3 /home/deamoon_uw_nn/uw-nn-adam/nn_train.py")
+os.system("python3 /home/deamoon_uw_nn/uw-nn-adam/nn_optimizer.py")
+os.system("python3 /home/deamoon_uw_nn/uw-nn-adam/nn_predict.py")
