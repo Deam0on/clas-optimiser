80a3988333ae18a33a7117a274011c351e8d9202
Deam0on <71432962+Deam0on@users.noreply.github.com>
2024-04-15 11:53:09 +0200

    Create nn_pull_param.py

---
+++ nn_pull_param.py
@@ -0,0 +1,26 @@
+import numpy as np
+import pandas as pd
+
+def extract_variables_from_csv(file_path):
+    # Load the CSV file
+    data = pd.read_csv(file_path, header=None, names=['Variable', 'Value'])
+    # Define the keys for each category
+    target_outputs_keys = ['M_V', 'M_N']
+    initial_guess_keys = ['F_total', 'AS/T', 'C(API)', 'C(SDS)', 'F_min']
+    bounds_keys = {'F_total','AS/T','C(API)','C(SDS)','F_min'}
+    
+    # Extract the target outputs
+    target_outputs = np.array([data[data['Variable'] == key]['Value'].astype(float).values[0] for key in target_outputs_keys])
+    # Extract the initial guess values
+    initial_guess = np.array([data[data['Variable'] == key]['Value'].astype(float).values[0] for key in initial_guess_keys])
+    # Extract bounds using the bounds_keys dictionary
+    bounds = [bounds_keys[key] for key in initial_guess_keys]
+    
+    return target_outputs, initial_guess, bounds
+
+
+file_path = '/home/deamoon_uw_nn/bucket_source/'
+target_outputs, initial_guess, bounds = extract_variables_from_csv(file_path)
+print("Target Outputs:", target_outputs)
+print("Initial Guess:", initial_guess)
+print("Bounds:", bounds)
