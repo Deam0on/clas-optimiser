06cf9146a854f2f44b02ea042404e353df53a837
Deam0on <71432962+Deam0on@users.noreply.github.com>
2024-04-15 10:50:46 +0200

    Update backup_sub_main.py
    opmized gsutil

--- backup_sub_main.py
+++ backup_sub_main.py
@@ -6,8 +6,8 @@ import shutil
 from distutils.dir_util import copy_tree
 
 # import datasets
-os.system("gcloud storage cp gs://uw-nn-storage_v2/Dataset/DATASET_T2.csv /home/deamoon_uw_nn/bucket_source")
-os.system("gcloud storage cp gs://uw-nn-storage_v2/Dataset/DATASET_V2.csv /home/deamoon_uw_nn/bucket_source")
+os.system("gsutil -m cp gs://uw-nn-storage_v2/Dataset/DATASET_T2.csv /home/deamoon_uw_nn/bucket_source")
+os.system("gsutil -m cp gs://uw-nn-storage_v2/Dataset/DATASET_V2.csv /home/deamoon_uw_nn/bucket_source")
 
 # remove & pull to update main
 dirpath = Path('/home/deamoon_uw_nn') / 'uw-nn-adam'
@@ -20,7 +20,7 @@ os.system("git clone https://github.com/Deam0on/uw-nn-adam.git")
 os.system("python3 /home/deamoon_uw_nn/uw-nn-adam/main.py")
 
 # update bucket - results to resuls (new) and archive all
-os.system("gsutil cp -r /home/deamoon_uw_nn/bucket_source gs://uw-nn-storage_v2/Results")
+os.system("gsutil -m cp -r /home/deamoon_uw_nn/bucket_source gs://uw-nn-storage_v2/Results")
 
 #create archive
 today = datetime.now()
@@ -29,5 +29,5 @@ os.mkdir(new_run)
 
 #copy and remove
 copy_tree("/home/deamoon_uw_nn/bucket_source", new_run)
-os.system("gsutil cp -r "+ new_run +" gs://uw-nn-storage_v2/Archive")
+os.system("gsutil -m cp -r "+ new_run +" gs://uw-nn-storage_v2/Archive")
 shutil.rmtree(new_run)
