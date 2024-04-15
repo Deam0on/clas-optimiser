a4ed66a8a0f532b4a7b4808a4138a5e7a3285315
Deam0on <71432962+Deam0on@users.noreply.github.com>
2024-04-15 13:15:30 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import os
 import sys
 ## placeholder
-os.system("python3 /home/deamoon_uw_nn/uw-nn-adam/nn_pull_param.py")
+# os.system("python3 /home/deamoon_uw_nn/uw-nn-adam/nn_pull_param.py")
 os.system("python3 /home/deamoon_uw_nn/uw-nn-adam/nn_train.py")
 os.system("python3 /home/deamoon_uw_nn/uw-nn-adam/nn_optimizer.py")
 os.system("python3 /home/deamoon_uw_nn/uw-nn-adam/nn_predict.py")
